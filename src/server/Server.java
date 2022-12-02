24009fd81686e7f9ff738aca90bf61e631512fff
Yuval zohar <yuvalzohar36@gmail.com>
2022-12-02 12:27:22 +0200

    update DB function & Handling response

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,7 @@
 package server;
 
 import gui.MainGUI;
-import models.IRequest;
-import models.Method;
-import models.Request;
-import models.Subscriber;
+import models.*;
 import ocsf.server.*;
 import serverModels.ServerConf;
 
@@ -14,104 +11,118 @@ import java.net.UnknownHostException;
 import java.util.List;
 
 /**
- * This class overrides some of the methods in the abstract 
+ * This class overrides some of the methods in the abstract
  * superclass in order to give more functionality to the server.
  */
 public class Server extends AbstractServer {
-  // The default port to listen on.
-  public mysqlController mysqlController;
-  private ServerConf currentConf;
-  
-  /**
-   * Constructs an instance of the server.
-   *
-   * @param port The port number to connect on.
-   */
-  public Server(int port) {
-    super(port);
-  }
-  
-  /**
-   * This method handles any messages received from the client.
-   *
-   * @param msg The message received from the client.
-   * @param client The connection from which the message originated.
-   */
-  public void handleMessageFromClient(Object msg, ConnectionToClient client) {
-      IRequest request = (Request)msg;
-      parseClientRequest(request);
+    // The default port to listen on.
+    public mysqlController mysqlController;
+    private ServerConf currentConf;
+
+    /**
+     * Constructs an instance of the server.
+     *
+     * @param port The port number to connect on.
+     */
+    public Server(int port) {
+        super(port);
+    }
+
+    /**
+     * This method handles any messages received from the client.
+     *
+     * @param msg    The message received from the client.
+     * @param client The connection from which the message originated.
+     */
+    public void handleMessageFromClient(Object msg, ConnectionToClient client) {
+        IRequest request = (Request) msg;
+        Response response = parseClientRequest(request);
+        try {
+            client.sendToClient(response);
+        } catch (IOException e) {
+            //handle exception
+            throw new RuntimeException(e);
+        }
+        {
+
+        }
 //	  System.out.println(this.getClientConnections()); // all clients
 //	  System.out.println("ip: " + client.getInetAddress().getHostAddress()); // this is ip
 //	  System.out.println("host: " + client.getInetAddress().getHostAddress());
 //	  System.out.println("status: " + client.isAlive()); // true if status is alive
 //	  System.out.println(client);
-	  //System.out.println("Message received: " + msg + " from " + client);
-	  //Map<String, String> userData = srialize((String) msg);
-	  //s.saveUserToDB(userData);
-	  //this.sendToAllClients(msg);
-  }
-
-  public void parseClientRequest(IRequest request){
-    String requestPath = request.getPath();
-    Method requestMethod = request.getMethod();
-    List<Object> requestBody = request.getBody();
-
-    switch (requestPath){
-      case "/UpdateSubscriber":
-        Subscriber subscriber = (Subscriber)requestBody.get(0);
-        if(requestMethod == Method.PUT) {
-          mysqlController.updateSubscriberCreditCardNumber(subscriber.getId(), subscriber.getCreditCardNumber());
-          mysqlController.updateSubscriberNumber(subscriber.getId(), subscriber.getSubscriberNumber());
-        }
-        else if(requestMethod == Method.GET)
-          System.out.println(mysqlController.getSubscriberDetails((String)requestBody.get(0)));
+        //System.out.println("Message received: " + msg + " from " + client);
+        //Map<String, String> userData = srialize((String) msg);
+        //s.saveUserToDB(userData);
+        //this.sendToAllClients(msg);
     }
-  }
-    
-  /**
-   * This method overrides the one in the superclass.
-   * Called when the server starts listening for connections.
-   */
-  protected void serverStarted() {
-    System.out.println("Server listening for connections on port " + getPort());
-    MainGUI.serverGui.printToConsole("Server listening for connections on port " + getPort());
-    mysqlController = new mysqlController(currentConf);
-    //models.Subscriber subscriber = new models.Subscriber("242", "55","2222","333","5","6",null);
-    //s.saveSubscriberToDB(subscriber);
-	//    s.updateSubscriberNumber("111", "123456789");
-	//    s.updateSubscriberNumber("3", null);
-	//    s.updateSubscriberNumber("6", "yuval");
-	//    models.Subscriber subscriber = s.getSubscriberDetails("111");
-	//    System.out.println(subscriber.getFirstName());
-	//    System.out.println(subscriber.getLastName());
-	//    System.out.println(subscriber.getId());
-	//    System.out.println(subscriber.getPhoneNumber());
-	//    System.out.println(subscriber.getEmailAddress());
-	//    System.out.println(subscriber.getCreditCardNumber());
-	//    System.out.println(subscriber.getSubscriberNumber());
-	//    models.Subscriber subscriber2 = s.getSubscriberDetails("123");
-	//    System.out.println(subscriber2.getFirstName());
-	//    System.out.println(subscriber2.getLastName());
-	//    System.out.println(subscriber2.getId());
-	//    System.out.println(subscriber2.getPhoneNumber());
-	//    System.out.println(subscriber2.getEmailAddress());
-	//    System.out.println(subscriber2.getCreditCardNumber());
-	//    System.out.println(subscriber2.getSubscriberNumber());
-	//    models.Subscriber subscriber3 = s.getSubscriberDetails("notexistid");
-	//    System.out.println(subscriber3);
-	//    System.out.println(s.isSubscriberExistInDB("notexistid"));
-	//    System.out.println(s.isSubscriberExistInDB("444"));
-	//    System.out.println(s.isSubscriberExistInDB("0"));    
-  }
-  
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server stops listening for connections.
-   */
-  protected void serverStopped() {
-    System.out.println("Server has stopped listening for connections.");
-  }  
-  
+
+    public Response parseClientRequest(IRequest request) {
+        String requestPath = request.getPath();
+        Method requestMethod = request.getMethod();
+        List<Object> requestBody = request.getBody();
+        Response response = new Response();
+
+        switch (requestPath) {
+            case "/UpdateSubscriber":
+                Subscriber subscriber = (Subscriber) requestBody.get(0);
+                if (requestMethod == Method.PUT) {
+                    mysqlController.updateSubscriberNumberAndCreditCard(subscriber.getId(), subscriber.getSubscriberNumber(), subscriber.getCreditCardNumber(), response);
+                }
+            case "/AllSubscribers":
+                if (requestMethod == Method.GET) {
+                    mysqlController.getAllSubscribersFromDB(response);
+                }
+        }
+        return response;
+    }
+
+
+    /**
+     * This method overrides the one in the superclass.
+     * Called when the server starts listening for connections.
+     */
+    protected void serverStarted() {
+        System.out.println("Server listening for connections on port " + getPort());
+        MainGUI.serverGui.printToConsole("Server listening for connections on port " + getPort());
+        mysqlController = new mysqlController(currentConf);
+        //mysqlController.updateSubscriberNumberAndSubscriberCreditCard("111", "0", "0");
+        //models.Subscriber subscriber = new models.Subscriber("242", "55","2222","333","5","6",null);
+        //s.saveSubscriberToDB(subscriber);
+        //    s.updateSubscriberNumber("111", "123456789");
+        //    s.updateSubscriberNumber("3", null);
+        //    s.updateSubscriberNumber("6", "yuval");
+        //    models.Subscriber subscriber = s.getSubscriberDetails("111");
+        //    System.out.println(subscriber.getFirstName());
+        //    System.out.println(subscriber.getLastName());
+        //    System.out.println(subscriber.getId());
+        //    System.out.println(subscriber.getPhoneNumber());
+        //    System.out.println(subscriber.getEmailAddress());
+        //    System.out.println(subscriber.getCreditCardNumber());
+        //    System.out.println(subscriber.getSubscriberNumber());
+        //    models.Subscriber subscriber2 = s.getSubscriberDetails("123");
+        //    System.out.println(subscriber2.getFirstName());
+        //    System.out.println(subscriber2.getLastName());
+        //    System.out.println(subscriber2.getId());
+        //    System.out.println(subscriber2.getPhoneNumber());
+        //    System.out.println(subscriber2.getEmailAddress());
+        //    System.out.println(subscriber2.getCreditCardNumber());
+        //    System.out.println(subscriber2.getSubscriberNumber());
+        //    models.Subscriber subscriber3 = s.getSubscriberDetails("notexistid");
+        //    System.out.println(subscriber3);
+        //    System.out.println(s.isSubscriberExistInDB("notexistid"));
+        //    System.out.println(s.isSubscriberExistInDB("444"));
+        //    System.out.println(s.isSubscriberExistInDB("0"));
+    }
+
+    /**
+     * This method overrides the one in the superclass.  Called
+     * when the server stops listening for connections.
+     */
+    protected void serverStopped() {
+        System.out.println("Server has stopped listening for connections.");
+    }
+
 //  /**
 //   * This method is responsible for the creation of the server instance.
 //   *
@@ -119,38 +130,38 @@ public class Server extends AbstractServer {
 //   * 		Defaults to 5555 if no argument is entered.
 //   */
 
-  public void closeServer() {
-    try {
-      this.close();
-    } catch (IOException e) {
-      System.out.println("Could not close server, because of:");
-      e.printStackTrace();
+    public void closeServer() {
+        try {
+            this.close();
+        } catch (IOException e) {
+            System.out.println("Could not close server, because of:");
+            e.printStackTrace();
+        }
     }
-  }
 
-  public static Server initServer(ServerConf serverConf) {
-    Server sv = new Server(serverConf.getPort());
-    sv.currentConf = serverConf;
+    public static Server initServer(ServerConf serverConf) {
+        Server sv = new Server(serverConf.getPort());
+        sv.currentConf = serverConf;
 
-    try {
-      sv.listen(); // Start listening for connections
+        try {
+            sv.listen(); // Start listening for connections
+        } catch (Exception ex) {
+            System.out.println("ERROR - Could not listen for clients!");
+        }
+        return sv;
     }
-    catch (Exception ex) {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
-    return sv;
-  }
 
-  public static ServerConf getDefaultServerConf() {
-    // default params
-    String ip = "127.0.0.1";
-    try {
-      ip = InetAddress.getLocalHost().getHostAddress();
-    } catch (UnknownHostException ignored) {}
-    int port = 5555;
-    String dbScheme = "world";
-    String dbUserName = "root";
-    String dbPassword = "1234";
-    return new ServerConf(ip, port, dbScheme, dbUserName, dbPassword);
-  }
+    public static ServerConf getDefaultServerConf() {
+        // default params
+        String ip = "127.0.0.1";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ignored) {
+        }
+        int port = 5555;
+        String dbScheme = "world";
+        String dbUserName = "root";
+        String dbPassword = "1234";
+        return new ServerConf(ip, port, dbScheme, dbUserName, dbPassword);
+    }
 }
--- src/server/mysqlController.java
+++ src/server/mysqlController.java
@@ -1,5 +1,7 @@
 package server;
 
+import models.IResponse;
+import models.Response;
 import models.Subscriber;
 import serverModels.ServerConf;
 
@@ -8,133 +10,157 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class mysqlController {
-	public Connection conn;
-	public mysqlController(ServerConf serverConf) {
-		String dbScheme = serverConf.getDbScheme();
-		String dbUserName = serverConf.getDbUserName();
-		String dbPassword = serverConf.getDbPassword();
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-			System.out.println("Driver definition succeed");
-		} catch (Exception ex) { // handle the error
-			System.out.println("Driver definition failed");
-		}
+    public Connection conn;
 
-		try {
-			conn = DriverManager.getConnection(String.format("jdbc:mysql://localhost/%s?serverTimezone=IST", dbScheme),
-											   dbUserName,
-											   dbPassword);
-			System.out.println("SQL connection succeed");
-			//printUsers(conn);
+    public mysqlController(ServerConf serverConf) {
+        String dbScheme = serverConf.getDbScheme();
+        String dbUserName = serverConf.getDbUserName();
+        String dbPassword = serverConf.getDbPassword();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            System.out.println("Driver definition succeed");
+        } catch (Exception ex) { // handle the error
+            System.out.println("Driver definition failed");
+        }
+
+        try {
+            conn = DriverManager.getConnection(String.format("jdbc:mysql://localhost/%s?serverTimezone=IST", dbScheme),
+                    dbUserName,
+                    dbPassword);
+            System.out.println("SQL connection succeed");
+            //printUsers(conn);
 //	        conn.close();
-		} catch (SQLException ex) { // handle any errors
-			System.out.println("SQLException: " + ex.getMessage());
-			System.out.println("SQLState: " + ex.getSQLState());
-			System.out.println("VendorError: " + ex.getErrorCode());
-		}
-	}
+        } catch (SQLException ex) { // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
 
-	 /**
-	 * this method get an Subscriber object and save his details to DB.
-	 * NOTE: Existing ID Subscriber OR null value except from subscriberNumber will raise an exception!
-	 */
-	  public void saveSubscriberToDB(Subscriber subscriber) {
-		PreparedStatement stmt;
-		System.out.println("Inserting Subscriber to DB");
-		String query = "INSERT into Subscriber VALUES (?, ?, ?, ?, ?, ?, ?)";
-		try {
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, subscriber.getFirstName());
-			stmt.setString(2, subscriber.getLastName());
-			stmt.setString(3, subscriber.getId());
-			stmt.setString(4, subscriber.getPhoneNumber());
-			stmt.setString(5, subscriber.getEmailAddress());
-			stmt.setString(6, subscriber.getCreditCardNumber());
-			stmt.setString(7, subscriber.getSubscriberNumber());
-			stmt.executeUpdate();
-			System.out.println("Subscriber update done successfully");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} 		
-	}
-	
-	public void updateSubscriberCreditCardNumber(String id, String newCreditCardNumber) {
-		/**
-		 * this method get id of subscriber and update his creditCardNumber to 'newCreditCardNumber' in DB.
-		 */
-		PreparedStatement stmt;
-		String query = "UPDATE Subscriber SET creditCardNumber= ? WHERE id= ?";
-		try {
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, newCreditCardNumber);
-			stmt.setString(2, id);
-			stmt.executeUpdate();
-			System.out.println("Subscriber update done successfully");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} 		
-	}
-	
-	public void updateSubscriberNumber(String id, String newSubscriberNumber) {
-		/**
-		 * this method get id of subscriber and update his subscriberNumber to 'newSubscriberNumber' in DB.
-		 */
-		PreparedStatement stmt;
-		String query = "UPDATE Subscriber SET SubscriberNumber= ? WHERE id= ?";
-		try {
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, newSubscriberNumber);
-			stmt.setString(2, id);
-			stmt.executeUpdate();
-			System.out.println("Subscriber update done successfully");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} 		
-	}
-	
-	public Subscriber getSubscriberDetails(String id) {
-		/**
-		 * this method get a subscriber id and return Subscriber object with all his details from the DB.
-		 * NOTE: method will return null if subscriber ID isn't exists in DB.
-		 */
-		//return null if Subscriber id does not exists
-		PreparedStatement stmt;
-		ResultSet rs;
-		String query = "SELECT * FROM Subscriber WHERE id = ?";
-	  	try 
-	  	{
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, id);
-			rs = stmt.executeQuery();
-	   		if(rs.next()) {
-	   			return new Subscriber(rs.getString("firstName"),rs.getString("lastName"),rs.getString("id")
-	   					,rs.getString("phoneNumber"),rs.getString("emailAddress"), rs.getString("creditCardNumber"), rs.getString("subscriberNumber"));
-	   		}
-	  		rs.close();
-	  	} catch (SQLException e) {e.printStackTrace();}
-		return null; //Subscriber id does not exists
-	}
+    /**
+     * this method get an Subscriber object and save his details to DB.
+     * NOTE: Existing ID Subscriber OR null value except from subscriberNumber will raise an exception!
+     */
+    public void saveSubscriberToDB(Subscriber subscriber) {
+        PreparedStatement stmt;
+        System.out.println("Inserting Subscriber to DB");
+        String query = "INSERT into Subscriber VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try {
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1, subscriber.getFirstName());
+            stmt.setString(2, subscriber.getLastName());
+            stmt.setString(3, subscriber.getId());
+            stmt.setString(4, subscriber.getPhoneNumber());
+            stmt.setString(5, subscriber.getEmailAddress());
+            stmt.setString(6, subscriber.getCreditCardNumber());
+            stmt.setString(7, subscriber.getSubscriberNumber());
+            stmt.executeUpdate();
+            System.out.println("Subscriber update done successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
-	public boolean isSubscriberExistInDB(String id) {
-		/**
-		 * this method get a subscriber id and return true/false if he is exists in DB or not
-		 */
-		PreparedStatement stmt;
-		ResultSet rs;
-		String query = "SELECT * FROM Subscriber WHERE id = ?";
-	  	try 
-	  	{
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, id);
-			rs = stmt.executeQuery();
-	   		if(rs.next()) {
-	   			return true;
-	   		}
-	  		rs.close();
-	  	} catch (SQLException e) {e.printStackTrace();}
-		return false; 
-	}
+    public void updateSubscriberNumberAndCreditCard(String id, String newSubscriberNumber, String newCreditCardNumber, Response response) {
+        /**
+         * this method get id of subscriber and update his subscriberNumber to 'newSubscriberNumber' in DB.
+         */
+        PreparedStatement stmt;
+        String query = "UPDATE Subscriber SET creditCardNumber= ?, SubscriberNumber= ? WHERE id= ?";
+        try {
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1, newCreditCardNumber);
+            stmt.setString(2, newSubscriberNumber);
+            stmt.setString(3, id);
+            stmt.executeUpdate();
+            System.out.println("Subscriber update done successfully");
+            editResponse(response, 200, "Successfully updated subscriber credentials", null);
+        } catch (SQLException e) {
+            editResponse(response, 404, "Error (FIX ACCORDING TO SPECIFIC EXCEPTION", null);
+            e.printStackTrace();
+        }
+    }
+
+    public Subscriber getSubscriberDetails(String id) {
+        /**
+         * this method get a subscriber id and return Subscriber object with all his details from the DB.
+         * NOTE: method will return null if subscriber ID isn't exists in DB.
+         */
+        //return null if Subscriber id does not exists
+        PreparedStatement stmt;
+        ResultSet rs;
+        String query = "SELECT * FROM Subscriber WHERE id = ?";
+        try {
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1, id);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Subscriber(rs.getString("firstName"), rs.getString("lastName"), rs.getString("id")
+                        , rs.getString("phoneNumber"), rs.getString("emailAddress"), rs.getString("creditCardNumber"), rs.getString("subscriberNumber"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null; //Subscriber id does not exists
+    }
+
+
+    public boolean isSubscriberExistInDB(String id) {
+        /**
+         * this method get a subscriber id and return true/false if he is exists in DB or not
+         */
+        PreparedStatement stmt;
+        ResultSet rs;
+        String query = "SELECT * FROM Subscriber WHERE id = ?";
+        try {
+            stmt = conn.prepareStatement(query);
+            stmt.setString(1, id);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public List<Object> getAllSubscribersFromDB(Response response) {
+        /**
+         * this method get a subscriber id and return true/false if he is exists in DB or not
+         */
+        Subscriber subscriber;
+        List<Object> subscribersList = new ArrayList<>();
+        PreparedStatement stmt;
+        ResultSet rs;
+        String query = "SELECT * FROM Subscriber";
+        try {
+            stmt = conn.prepareStatement(query);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                subscriber = new Subscriber(rs.getString("firstName"), rs.getString("lastName"), rs.getString("id")
+                        , rs.getString("phoneNumber"), rs.getString("emailAddress"), rs.getString("creditCardNumber"), rs.getString("subscriberNumber"));
+                subscribersList.add(subscriber);
+                editResponse(response, 200, "Successfully sent all subscribers", subscribersList);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            editResponse(response, 404, "Error (FIX ACCORDING TO SPECIFIC EXCEPTION", null);
+            e.printStackTrace();
+        }
+        return subscribersList;
+    }
+
+    void editResponse(Response response, int code, String description, List<Object> body) {
+        response.setBody(body);
+        response.setCode(code);
+        response.setDescription(description);
+    }
 }
